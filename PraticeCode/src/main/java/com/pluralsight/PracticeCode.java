99ef95726659b16f245271c94e659b735eac4962
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-24 01:55:51 -0500

    The basis

--- PraticeCode/src/main/java/com/pluralsight/PracticeCode.java
+++ PraticeCode/src/main/java/com/pluralsight/PracticeCode.java
@@ -5,9 +5,30 @@ import java.util.Date;
 
 public class PracticeCode {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
+        sayHello();             //Method 1
+        playWithPoints();       //Method 2
+        sayGoodbye();           //Method 3
+        printCurrentDate();     //Method 4
+    }
 
+
+    public static void sayHello() {
+        System.out.println("Hello You");
+    }
+    public static void playWithPoints() {
+        Point point1 = new Point(1, 1);
+        Point point2= point1;
+        point1.x = 2;
+        System.out.println(point2);
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Keep it moving mate!");
+    }
+
+    public static void printCurrentDate() {
+        Date now = new Date();
+        System.out.println( now);
     }
 
 
