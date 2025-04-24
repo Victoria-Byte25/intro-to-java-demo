ce6e6fe4843394afe38245306be3d25055879afe
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-24 00:45:48 -0500

    Practice

---
+++ PraticeCode/src/main/java/com/pluralsight/PracticeCode.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.awt.*;
+
+public class PracticeCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+
+
+
+
+
+
+}
